0e195fb98a453b6fa574ec5448a76182eceb243f
ashl1future@gmail.com <ashl1future@gmail.com@d2f1cfbf-7c20-5a46-3a0a-290d5e63a1b4>
2011-04-18 07:54:01 +0000

    Add GUI support for plugin Skype Linux in Linux

--- impi-skype-linux/trunk/skype-linux.cpp
+++ impi-skype-linux/trunk/skype-linux.cpp
@@ -12,13 +12,46 @@ PluginSkypeLinux::~PluginSkypeLinux(){
 
 }
 
+/* virtual */ QString PluginSkypeLinux::Version() const{
+	return QString("0.1");
+}
+
+/* virtual */ QString PluginSkypeLinux::FullName() const{
+	return QString("Skype Linux");
+}
+/* virtual */ QString PluginSkypeLinux::MajorName() const{
+	return QString("Skype");
+}
+/* virtual */ QString PluginSkypeLinux::MinorName() const{
+	return QString("Linux");
+}
+
 /* virtual */ bool PluginSkypeLinux::CanInitFromFile() const{
 	return false;
 }
 
 /* virtual */ void PluginSkypeLinux::InitFromConfPath(QDir* path){
-	int a;
-	a = 10;
+
+}
+
+/* virtual */ void PluginSkypeLinux::InitFromFile(QDir* path, QString* filename){
+	throw Error(1);
+}
+
+/* virtual */ void PluginSkypeLinux::Accounts(quint32 count, quint32 from, QObject* accounts){
+	;
+}
+
+/* virtual */ void PluginSkypeLinux::Clients(quint32 count, quint32 from, QObject* clients){
+	;
+}
+
+/* virtual */ void PluginSkypeLinux::Messages(quint64 count, quint64 from, QObject* messages){
+	;
+}
+
+/* virtual */ void PluginSkypeLinux::Users(quint32 count, quint32 from, QObject* users){
+	;
 }
 
 Q_EXPORT_PLUGIN2(impi-skype-linux, PluginSkypeLinux)
--- impi-skype-linux/trunk/skype-linux.h
+++ impi-skype-linux/trunk/skype-linux.h
@@ -11,19 +11,44 @@
 #include <QObject>
 #include "../impi/client-interface.h"
 
-class PluginSkypeLinux : public QObject, public ClientInterface {
+class AccountSkypeLinux : public Account {
+
+};
+
+class ClientSkypeLinux : public Client {
+
+};
+
+class MessageSkypeLinux : public Message {
+
+};
+
+class UserSkypeLinux : public User {
+
+};
+
+class PluginSkypeLinux: public QObject, public PluginInterface {
 Q_OBJECT
-Q_INTERFACES(ClientInterface)
+Q_INTERFACES(PluginInterface)
 
 private:
 
 public:
 	virtual ~PluginSkypeLinux();
 
+	virtual QString Version() const;
+	virtual QString FullName() const;
+	virtual QString MajorName() const;
+	virtual QString MinorName() const;
 	virtual bool CanInitFromFile() const;
 
 	virtual void InitFromConfPath(QDir* path);
+	virtual void InitFromFile(QDir* path, QString* filename);
 
+	virtual void Accounts(quint32 count, quint32 from, QObject* accounts);
+	virtual void Clients(quint32 count, quint32 from, QObject* clients);
+	virtual void Messages(quint64 count, quint64 from, QObject* messages);
+	virtual void Users(quint32 count, quint32 from, QObject* users);
 };
 
 #endif /* SKYPELINUX_H_ */
